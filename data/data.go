6aab5be5dd0f2782015676ddf5de6a7ea83870fb
Yasuyuki ENDO <eyasuyuki@gmail.com>
2021-10-27 23:14:57 +0900

    remove unuse options

--- data/data.go
+++ data/data.go
@@ -13,20 +13,14 @@ func CreateData() *Data {
 
 func CreateConfig() *Config {
 	return &Config{
-		HorizontalResolution: 96.0, // 96 dpi
 		VerticalResolution:   96.0, // 96 dpi
-		RowHeight:            6.61, // 18.75pt
-		MaxWidth:             190,  // 210 - (10 x 2)
 		UseImageHeight:       true,
 		ImgScale:             1.0,
 	}
 }
 
 type Config struct {
-	HorizontalResolution float64 `yaml:"horizontal_resolution"`
 	VerticalResolution   float64 `yaml:"vertical_resolution"`
-	RowHeight            float64 `yaml:"row_height"`
-	MaxWidth             float64 `yaml:"max_width"`
 	UseImageHeight       bool    `yaml:"use_image_height"`
 	ImgScale             float64 `yaml:"img_scale""`
 }
@@ -49,5 +43,4 @@ type Row struct {
 type Col struct {
 	Text string `yaml:"text"`
 	Img  string `yaml:"img"`
-	Pdf  string `yaml:"pdf"`
 }
